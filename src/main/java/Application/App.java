11b8de3a50446248f948c8ef1fd60f02e0aac62e
LaOwlLol <something.less.than.nate@gmail.com>
2018-08-08 22:29:42 -0500

    added select layout button

--- src/main/java/Application/App.java
+++ src/main/java/Application/App.java
@@ -20,32 +20,74 @@
 package Application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class App extends Application {
 
+    Stage appStage;
+    BorderPane rootPane;
+    Button fileSelectBtn;
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root;
+        Parent customLayout;
+        final FileChooser fileChooser = new FileChooser();
+
+        rootPane = new BorderPane();
+        appStage = stage;
+        fileSelectBtn = new Button("Select Layout");
+        fileSelectBtn.setOnAction( (event) -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                setLayout(file.toPath());
+            }
+        } );
+
+        rootPane.setBottom(fileSelectBtn);
 
         if (Files.exists(Paths.get(System.getProperty("user.home"), "GuiPrototype.fxml"))) {
-            root = FXMLLoader.load(Paths.get(System.getProperty("user.home"), "GuiPrototype.fxml").toUri().toURL());
-        }
-        else {
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("defaultLayout.fxml"));
+            setLayout(Paths.get(System.getProperty("user.home"), "GuiPrototype.fxml"));
         }
 
-        if (root != null) {
-            Scene scene = new Scene(root);
-            stage.setTitle("GUI Prototype");
-            stage.setScene(scene);
-            stage.show();
+        else {
+            customLayout = FXMLLoader.load(getClass().getClassLoader().getResource("defaultLayout.fxml"));
+            rootPane.setCenter(customLayout);
+        }
+
+        Scene scene = new Scene(rootPane);
+        stage.setTitle("GUI Prototype");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    private void setLayout(Path path) {
+        Parent customLayout = null;
+
+        try {
+            customLayout = FXMLLoader.load(path.toUri().toURL());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (customLayout != null) {
+            rootPane.setCenter(customLayout);
         }
     }
 }
